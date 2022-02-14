3e0e7d88a4be8964741f54b9e27499a44d43a62b
emilykidd3 <e.kidd61@yahoo.com>
2022-02-13 22:41:05 -0800

    added add segments funciton to increase snake length when it eats the food

--- food.js
+++ food.js
@@ -6,7 +6,7 @@ const EXPANSION_RATE = 1
 export function update() {
     if (onSnake(food)) {
         expandSnake(EXPANSION_RATE)
-        food = { x: 20, y: 10 }
+        food = { x: 20, y: 10 } 
     }
 }
 
