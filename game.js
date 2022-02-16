a312cf30f8fc94d78213979d8956ea2a0cc5f07d
emilykidd3 <e.kidd61@yahoo.com>
2022-02-15 20:00:36 -0800

    added fucntions to get location of snake head, see if it intersected with its body, and if the snake ran into a wall

--- game.js
+++ game.js
@@ -1,5 +1,6 @@
-import { update as updateSnake, draw as drawSnake, SNAKE_SPEED } from './snake.js'
+import { update as updateSnake, draw as drawSnake, SNAKE_SPEED, getSnakeHead, snakeIntersection } from './snake.js'
 import { update as updateFood, draw as drawFood } from './food.js'
+import { outsideGrid } from './grid.js'
 
 let lastRenderTime = 0;
 let gameOver = false;
@@ -8,7 +9,7 @@ const gameBoard = document.getElementById('game-board');
 
 function main(currentTime) {
     if (gameOver) {
-        alert("you lose!")
+        return alert("you lose!")
     }
 
     window.requestAnimationFrame(main)
